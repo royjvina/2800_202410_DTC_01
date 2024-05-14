1869f4f197b5ff83be2177674af86bc37006d339
Balpreet Sekhon <balpreetsekhon787@gmail.com>
2024-05-14 09:09:27 -0700

    Add functionality to friends and groups tab

---
+++ public/scripts/homepage.js
@@ -0,0 +1,44 @@
+/**
+ * This script is used to handle homepage javascript functionalities
+ */
+
+/**
+ * This function is used to handle the click event on the groups tab    
+ * @balpreet787
+ */
+function groupsTabHandler() {
+    showFriends.classList.remove('bg-[#4b061a]');
+    showFriends.classList.remove('text-white');
+    showGroups.classList.add('bg-[#4b061a]');
+    showGroups.classList.add('text-white');
+    groupSearch.classList.remove('hidden');
+    friendSearch.classList.add('hidden');
+    friends.classList.add('hidden');
+    groups.classList.remove('hidden');
+    groups.classList.add('flex');
+    addFriendSecondaryDiv.classList.add('hidden');
+    addGroupSecondaryDiv.classList.remove('hidden');
+    addGroupSecondaryDiv.classList.add('flex');
+}
+/**
+ * This function is used to handle the click event on the friends tab    
+ * @balpreet787
+ */
+function friendsTabHandler() {
+    showGroups.classList.remove('bg-[#4b061a]');
+    showGroups.classList.remove('text-white');
+    showFriends.classList.add('bg-[#4b061a]');
+    showFriends.classList.add('text-white');
+    groupSearch.classList.add('hidden');
+    friendSearch.classList.remove('hidden');
+    groups.classList.add('hidden');
+    friends.classList.remove('hidden');
+    friends.classList.add('flex');
+    addGroupSecondaryDiv.classList.add('hidden');
+    addFriendSecondaryDiv.classList.remove('hidden');
+    addFriendSecondaryDiv.classList.add('flex');
+}
+
+
+showGroups.addEventListener('click', groupsTabHandler);
+showFriends.addEventListener('click', friendsTabHandler);
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('main');
 });
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
