970a35cb7338503e387565cc960f04c3c3030180
Claudia Le <cle54@my.bcit.ca>
2024-05-15 12:51:18 -0700

    Complete css for adding expenses by percentage and manual

--- public/scripts/addExpenses.js
+++ public/scripts/addExpenses.js
@@ -3,42 +3,60 @@
  */
 
 /**
- * This function is used to handle the click event on the equal expenses tab    
+ * This function is used to handle the click event on the split expenses equal tab    
  * @claaudiaale
  */
-function groupsTabHandler() {
-    showFriends.classList.remove('bg-[#4b061a]');
-    showFriends.classList.remove('text-white');
-    showGroups.classList.add('bg-[#4b061a]');
-    showGroups.classList.add('text-white');
-    groupSearch.classList.remove('hidden');
-    friendSearch.classList.add('hidden');
-    friends.classList.add('hidden');
-    groups.classList.remove('hidden');
-    groups.classList.add('flex');
-    addFriendSecondaryDiv.classList.add('hidden');
-    addGroupSecondaryDiv.classList.remove('hidden');
-    addGroupSecondaryDiv.classList.add('flex');
+function equalExpenseTabHandler(event) {
+    event.preventDefault();
+    showPercentageExpense.classList.remove('bg-[#4b061a]');
+    showPercentageExpense.classList.remove('text-white');
+    showManualExpense.classList.remove('bg-[#4b061a]');
+    showManualExpense.classList.remove('text-white');
+    showEqualExpense.classList.add('bg-[#4b061a]');
+    showEqualExpense.classList.add('text-white');
+    splitExpensePercentage.classList.add('hidden');
+    splitExpenseManually.classList.add('hidden');
+    splitExpenseEqually.classList.remove('hidden');
+    splitExpenseEqually.classList.add('flex-col');
 }
+
 /**
- * This function is used to handle the click event on the friends tab    
- * @balpreet787
+ * This function is used to handle the click event on the split expenses by percentage tab    
+ * @claaudiaale
  */
-function friendsTabHandler() {
-    showGroups.classList.remove('bg-[#4b061a]');
-    showGroups.classList.remove('text-white');
-    showFriends.classList.add('bg-[#4b061a]');
-    showFriends.classList.add('text-white');
-    groupSearch.classList.add('hidden');
-    friendSearch.classList.remove('hidden');
-    groups.classList.add('hidden');
-    friends.classList.remove('hidden');
-    friends.classList.add('flex');
-    addGroupSecondaryDiv.classList.add('hidden');
-    addFriendSecondaryDiv.classList.remove('hidden');
-    addFriendSecondaryDiv.classList.add('flex');
+function percentageExpenseTabHandler(event) {
+    event.preventDefault();
+    showManualExpense.classList.remove('bg-[#4b061a]');
+    showManualExpense.classList.remove('text-white');
+    showEqualExpense.classList.remove('bg-[#4b061a]');
+    showEqualExpense.classList.remove('text-white');
+    showPercentageExpense.classList.add('bg-[#4b061a]');
+    showPercentageExpense.classList.add('text-white');
+    splitExpenseEqually.classList.add('hidden');
+    splitExpenseManually.classList.add('hidden');
+    splitExpensePercentage.classList.remove('hidden');
+    splitExpensePercentage.classList.add('flex-col');
+}
+
+/**
+ * This function is used to handle the click event on the split expenses by percentage tab    
+ * @claaudiaale
+ */
+function manualExpenseTabHandler(event) {
+    event.preventDefault();
+    showPercentageExpense.classList.remove('bg-[#4b061a]');
+    showPercentageExpense.classList.remove('text-white');
+    showEqualExpense.classList.remove('text-white');
+    showEqualExpense.classList.remove('bg-[#4b061a]');
+    showManualExpense.classList.add('bg-[#4b061a]');
+    showManualExpense.classList.add('text-white');
+    splitExpenseEqually.classList.add('hidden');
+    splitExpensePercentage.classList.add('hidden');
+    splitExpenseManually.classList.remove('hidden');
+    splitExpenseManually.classList.add('flex-col');
 }
 
 
-showGroups.addEventListener('click', groupsTabHandler);
-showFriends.addEventListener('click', friendsTabHandler);
+showEqualExpense.addEventListener('click', function(event){equalExpenseTabHandler(event)});
+showPercentageExpense.addEventListener('click', function(event){percentageExpenseTabHandler(event)});
+showManualExpense.addEventListener('click', function(event){manualExpenseTabHandler(event)});
