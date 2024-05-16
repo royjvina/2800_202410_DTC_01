f30314174ebf220effd32357b6fb68fb5b0ff66e
Claudia Le <cle54@my.bcit.ca>
2024-05-16 12:53:13 -0700

    Add apexcharts API for group balances in balances tab

--- public/scripts/groups.js
+++ public/scripts/groups.js
@@ -35,3 +35,113 @@ function balancesTabHandler() {
 
 showExpenses.addEventListener('click', expensesTabHandler);
 showBalances.addEventListener('click', balancesTabHandler);
+
+
+// chart API for balances
+const data = [
+    {x: 'User 1', y: -41.38, color: '#FF4560'},
+    {x: 'User 2', y: -70.50, color: '#FF4560'}, 
+    {x: 'User 3', y: 103.50, color: '#00E396'}, 
+    {x: 'User 4', y: 13.30, color: '#00E396'},  
+    {x: 'User 5', y: 0, color: '#00E396'},   
+    {x: 'User 6', y: -28.70, color: '#FF4560'}, 
+    {x: 'User 7', y: 25, color: '#00E396'}, 
+];
+
+var options = {
+    chart: {
+        type: 'bar', 
+        height: 400, 
+        toolbar: {
+            show: false 
+        },
+        style: {
+            marginLeft: 50,
+            marginRight: 50
+        }
+    },
+    plotOptions: {
+        bar: {
+            borderRadius: 6, 
+            borderRadiusApplication: 'end', 
+            horizontal: true, 
+            barHeight: '50%', 
+            distributed: true,
+            dataLabels: {
+                position: 'bottom' 
+            }
+        }
+    },
+    dataLabels: {
+        enabled: true, 
+        formatter: function (val) {
+            return val < 0 ? `-$${Math.abs(val).toFixed(2)}` : `$${val.toFixed(2)}`; 
+        },        
+        offsetX: -50,
+        style: {
+            fontSize: '14px', 
+            colors: ['#000'], 
+            fontWeight: 'medium' 
+        }
+    },
+    stroke: {
+        show: false, 
+    },
+    series: [{
+        name: '',
+        data: data.map(item => ({x: item.x, y: item.y, fillColor: item.color}))
+    }],
+    xaxis: {
+        categories: data.map(item => item.x), 
+        labels: {
+            show: false, 
+            formatter: function (val) {
+                return `$${Math.abs(val)}`; 
+            }
+        },
+        axisBorder: {
+            show: false
+        },
+        axisTicks: {
+            show: false 
+        }
+    },
+    yaxis: {
+        title: {
+            text: undefined
+        },
+        labels: {
+            style: {
+                colors: ['#000'] ,
+                fontWeight: 'bold',
+                fontSize: '14px'
+            }
+        }
+    },
+    fill: {
+        type: 'solid',
+        colors: data.map(item => item.color), 
+        opacity: 1 
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val < 0 ? `-$${Math.abs(val)}` : `$${val}`; 
+            }
+        }
+    },
+    grid: {
+        borderColor: '#f1f1f1', 
+        xaxis: {
+            lines: {
+                show: false 
+            }
+        }
+    },
+    legend: {
+        show: false
+    },
+};
+
+var chart = new ApexCharts(document.querySelector("#balances"), options);
+chart.render();
