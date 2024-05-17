49b297f7508636cafc45299314dbbd8dc4d80554
royjvina <roy.jeph@gmail.com>
2024-05-16 19:03:57 -0700

    Implement 'forgotten password' functionality

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     phone: { type: String, required: true },
     password: { type: String, required: true },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
 });
 
 module.exports = mongoose.model('User', userSchema);
