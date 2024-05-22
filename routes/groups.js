47ac9235de68ba30416a83e0c8b89023ec5a797a
Claudia Le <cle54@my.bcit.ca>
2024-05-21 17:36:38 -0700

    Display groups and users from server side

--- routes/groups.js
+++ routes/groups.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/groups', (req, res) => {
-    res.render('groups', { path: '/home' })
+    res.render('groups', { path: '/home', groupName: 'Groups', usersInGroup: ['User 1', 'User 2', 'User 3', 'User 4'], })
 })
 
 
