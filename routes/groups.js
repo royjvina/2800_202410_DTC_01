6dd284531a2d0adb9ee48cea4d39f84a3e0742ce
Claudia Le <cle54@my.bcit.ca>
2024-05-23 12:27:05 -0700

    Display group expenses and their information on group's page

--- routes/groups.js
+++ routes/groups.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const Group = require('../models/Group');
-
+const Transaction = require('../models/Transaction');
 
 
 router.get('/groups', async (req, res) => {
     let groupId = req.query.groupId;
-    let group = await Group.findOne({ _id: groupId }).populate('members.user_id');
+    let group = await Group.findOne({ _id: groupId }).populate('members.user_id').populate('transactions');
     if (group.group_pic && group.group_pic.data) {
     group.group_picBase64 = `data:${group.group_pic.contentType};base64,${group.group_pic.data.toString('base64')}`;
     }
@@ -16,11 +16,11 @@ router.get('/groups', async (req, res) => {
 
 // router.get('/individualExpense', async (req, res) => {
 //     let groupId = req.query.groupId;
-//     let group = await Group.findOne({ _id: groupId }).populate('members.user_id');
-//     let expenseId = req.query.expenseId;
-//     let expense = await Transaction.findOne({ _id: expenseId });
+//     let group = await Group.findOne({ _id: groupId }).populate('transactions._id');
+//     // let transactionId = req.query.transactionsId;
+//     // let transactions = await Transaction.find({ _id: transactionId }).populate('transactions._id');
 
-//     res.render('individualExpense', { path: '/groups', expense: expense })
+//     res.render('individualExpense', { path: '/groups', group: group});
 // })
 
 
